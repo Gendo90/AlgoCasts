6c9fece3615d8453d3662f50f8846d8a7d3afff6
Patrick Gendotti <patrick.gendotti@gmail.com>
2019-01-31 16:50:16 -0800

    feat: Create recursive solution to 'reversestring' problem - took about 1.5 hours
    Note: Could also make a new string after iterating through the original
    string in reverse order, may be an easier/quicker solution, but the
    course says sometimes the interviewers like to see recursion!

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -2,10 +2,32 @@
 // Given a string, return a new string with the reversed
 // order of characters
 // --- Examples
-//   reverse('apple') === 'leppa'
+//   reverse('apple') === 'elppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
 
+  if(str.length!==1) {
+    var last_letter = str[0]
+    var reversed = ''
+    var shortened_str = str.substring(0, (str.length-1))  + ""
+    if(shortened_str!=="") {
+      reversed += str[str.length-1]
+      reversed += reverse(shortened_str)
+      if(str.length-1!==1){
+      return reversed}
+      else{
+        return reversed+last_letter
+      }}}
+  else {
+    console.log('reached the end!')
+    var reversed = ''
+    return reversed
+  }
+
+}
+
+//console.log(reverse('apple'))
+//console.log('HELLO')
 module.exports = reverse;
